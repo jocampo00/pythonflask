8981781e06f59b2e38e7f21791eb298a6413f239
jocampo00 <116417715+jocampo00@users.noreply.github.com>
2024-05-13 13:41:15 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,34 +1,50 @@
 from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
-  
+swagger = Swagger(app)
+
 # Sample data for artists, songs, and albums
 artists = [
     {"id": 1, "name": "Vincent van Gogh"},
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
+
 songs = [
     {"id": 1, "name": "ViAJAA dovan Gogh"},
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
+
 # Routes for querying artists, songs, and albums
-@app.route('/artists')
+@app.route('/artists', methods=['GET'])
 def get_artists():
+    """
+    Retrieve a list of artists.
+    ---
+    responses:
+      200:
+        description: A list of artists.
+    """
     return jsonify(artists)
-@app.route('/songs')
+
+@app.route('/songs', methods=['GET'])
 def get_songs():
+    """
+    Retrieve a list of songs.
+    ---
+    responses:
+      200:
+        description: A list of songs.
+    """
     return jsonify(songs)
 
 if __name__ == '__main__':
     # Set the port to 80
     port = 80
+    app.run(host='0.0.0.0', port=port)
 
-    
-          
-            
-    
 
           
           Expand Down
