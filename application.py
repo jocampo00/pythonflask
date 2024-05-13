870f16aeaf017011729e6e372ec880f7cfd584af
jocampo00 <116417715+jocampo00@users.noreply.github.com>
2024-05-13 12:58:47 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,25 +1,22 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-
+  
 # Sample data for artists, songs, and albums
 artists = [
     {"id": 1, "name": "Vincent van Gogh"},
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
-
 songs = [
     {"id": 1, "name": "ViAJAA dovan Gogh"},
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
-
 # Routes for querying artists, songs, and albums
 @app.route('/artists')
 def get_artists():
     return jsonify(artists)
-
 @app.route('/songs')
 def get_songs():
     return jsonify(songs)
@@ -27,4 +24,15 @@ def get_songs():
 if __name__ == '__main__':
     # Set the port to 80
     port = 80
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
     app.run(host='0.0.0.0', port=port)
