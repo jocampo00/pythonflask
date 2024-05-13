5dc37e28f4bceb6b0bd0a28cf861e092fe2220ec
jocampo00 <116417715+jocampo00@users.noreply.github.com>
2024-05-13 17:26:44 -0500

    Update application.py

--- application.py
+++ application.py
@@ -8,36 +8,25 @@ artists = [
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
+
 songs = [
     {"id": 1, "name": "ViAJAA dovan Gogh"},
     {"id": 2, "name": "Pablo Picasso"},
     {"id": 3, "name": "Leonardo da Vinci"}
 ]
+
 # Routes for querying artists, songs, and albums
-@app.route('/artists', methods=['GET'])
-  def get_artists():
-    """
-    Retrieve a list of artists.
-    ---
-    responses:
-      200:
-        description: A list of artists.
-    """
+@app.route('/artists')
+def get_artists():
     return jsonify(artists)
-@app.route('/songs', methods=['GET'])
-  def get_songs():
-    """
-    Retrieve a list of songs.
-    ---
-    responses:
-      200:
-        description: A list of songs.
-    """
+
+@app.route('/songs')
+def get_songs():
     return jsonify(songs)
 
 if __name__ == '__main__':
     # Set the port to 80
     port = 80
-    
+    app.run(host='0.0.0.0', port=port)
     
   
