0d7d7b4294993fe06313363f8fdbd59dceed3477
Jessica A Porras <javierocampoacero@gmail.com>
2024-05-13 10:20:13 -0500

    flask

---
+++ mundoloc.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Sample data for artists, songs, and albums
+artists = [
+    {"id": 1, "name": "Vincent van Gogh"},
+    {"id": 2, "name": "Pablo Picasso"},
+    {"id": 3, "name": "Leonardo da Vinci"}
+]
+# sample
+
+
+# Routes for querying artists, songs, and albums
+@app.route('/artists')
+def get_artists():
+    return jsonify(artists)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
